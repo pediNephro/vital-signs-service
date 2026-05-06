24bd99add4a7d7ee1c723fafaea480e6b397da48
AzizTurki07 <torkich7@gmail.com>
2026-05-06 18:20:35 +0100

    testt

--- src/test/java/tn/esprit/vitalsigns/VitalSignsApplicationTests.java
+++ src/test/java/tn/esprit/vitalsigns/VitalSignsApplicationTests.java
@@ -4,16 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(properties = {
-    "eureka.client.enabled=false",
-    "spring.cloud.discovery.enabled=false",
-    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration"
-})
-@ActiveProfiles("test")
+
 class VitalSignsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
